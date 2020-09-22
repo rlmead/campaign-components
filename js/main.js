1ec6cc819c3862b70a98d8d772e540586c8c8a04
Rebecca Mead <beckyleemead@gmail.com>
2020-09-22 13:55:39 -0400

    js pseudocoding

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// get data from viewpoints.json (onload?)
+
+// function to populate cards with viewpoints
+
+    // iterate through objects in viewpoints.json main array
+
+        // const new_card = document.createElement('div');
+
+        // newCard.setAttribute('class', ... all the bootstrap stuff)
+
+        // insert subject, blurb, and icon into the card
